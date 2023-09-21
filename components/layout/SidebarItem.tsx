f28ba23dc1cb89e8e394a876fd4bbaa139f27773
JonathanDabre <jonathandabre01@gmail.com>
2023-09-21 23:01:14 +0530

    fixes in sidebar

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -23,12 +23,12 @@ const SidebarItem: React.FC<SidebarItemProps> = (
 ) => {
   return (
     <div className="flex flex-row items-center">
-        <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer lg:hidden">
+        <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer lg:hidden ">
             <Icon size={28} color='white'/>
         </div>
-        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-blue-300 hover:opacity-10 cursor-pointer">
-            <Icon size={24} color='white' />
-            <p className="hidden lg:block text-white text-xl">
+        <div className="relative hidden lg:flex items-center gap-4 p-4 rounded-full hover:bg-blue-300 hover:bg-opacity-10 cursor-pointer">
+            <Icon size={24} color='white' className='' />
+            <p className="hidden lg:block text-white text-xl ">
                 {label}
             </p>
         </div>
