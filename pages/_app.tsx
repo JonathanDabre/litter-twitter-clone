2a07d7b4a53536b5921b35c7df91c395a4f56655
Alroy <alroypereira05@gmail.com>
2023-09-21 18:39:40 +0530

    auth fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import Layout from '../components/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
 import RegisterModal from '@/components/modals/RegisterModal'
 import LoginModal from '@/components/modals/LoginModal'
 
