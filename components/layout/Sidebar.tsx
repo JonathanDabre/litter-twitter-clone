edace83ace8428260849315215a335afa59cc8fd
JonathanDabre <jonathandabre01@gmail.com>
2023-09-21 10:36:14 +0530

    Added react icons

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {BsHouseFill} from 'react-icons/bs';
 
 const Sidebar = () => {
+
+  const items = [
+    {
+      label: 'Home',
+      href: '/',
+      icon: BsHouseFill
+    }
+  ]
+
   return (
     <div>Sidebar</div>
   )
